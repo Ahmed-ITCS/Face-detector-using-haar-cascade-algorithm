fb4bd048b794487cc7da96bdb789926292bb3654
Ahmed-ITCS <59608481+Ahmed-ITCS@users.noreply.github.com>
2021-06-23 04:19:07 +0500

    Add files via upload

---
+++ facedetection.py
@@ -0,0 +1,32 @@
+import cv2
+from random import  randrange
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+#img = cv2.imread('maya.jpg')
+webcam =cv2.VideoCapture(0)
+while True:
+    # Silicon.Valley.S01E01labiba marja.720p.5.1Ch.BluRay.ReEnc-DeeJayAhmed.mkv
+    successful_frame_read, frame =webcam.read()
+
+    greyscale_img = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+
+    facelocation = trained_face_data.detectMultiScale(greyscale_img)
+
+    for (x, y, w, z) in facelocation:
+        cv2.rectangle(frame, (x, y), (x + w, y + z), (0,255,0), 3)
+
+    cv2.imshow("check", frame)
+    cv2.waitKey(1)
+"""""
+facelocation  = trained_face_data.detectMultiScale(greyscale_img)
+
+for (x,y,w,z) in facelocation:
+    cv2.rectangle(img,(x,y),(x+w,y+z),(0,255,0),1)
+
+cv2.imshow("check",img)
+print(facelocation)
+
+cv2.waitKey()
+"""""
+print("done")
